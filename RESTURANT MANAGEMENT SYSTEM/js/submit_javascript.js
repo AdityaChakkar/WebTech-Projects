6473ea3b9548773946528e49e44039ff970a0c95
Aditya Chakkar <130596876+AdityaChakkar@users.noreply.github.com>
2024-03-14 22:25:24 +0530

    Add files via upload

---
+++ RESTURANT MANAGEMENT SYSTEM/js/submit_javascript.js
@@ -0,0 +1,7 @@
+// Submit form with HTML <form> tag function.
+function submit_by_tag() {
+var x = document.getElementsByTagName("form");
+x[0].submit(); //form submission
+alert("Send Successfully......");
+
+}
